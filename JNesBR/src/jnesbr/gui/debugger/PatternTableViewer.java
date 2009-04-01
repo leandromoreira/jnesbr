daa30c8964f679fa7ec236cd7bcc7cfba70a1b22
leandro.rhcp99 <leandro.rhcp99@4cb0bef4-e3dc-11dd-9759-139c9ab14fc6>
2009-04-01 00:10:34 +0000

    initial pattern table step forward on rom file. some illegal opcodes.

--- JNesBR/src/jnesbr/gui/debugger/PatternTableViewer.java
+++ JNesBR/src/jnesbr/gui/debugger/PatternTableViewer.java
@@ -14,11 +14,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with JNesBR.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package jnesbr.gui.debugger;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import jnesbr.core.Emulator;
@@ -29,6 +29,8 @@ import jnesbr.core.Emulator;
 public class PatternTableViewer extends javax.swing.JFrame {
 
     private static Map<Integer, Color> colors = new HashMap<Integer, Color>();
+
+
     static {
         colors.put(0, Color.WHITE);
         colors.put(1, Color.RED);
@@ -46,6 +48,8 @@ public class PatternTableViewer extends javax.swing.JFrame {
 
         jPnPatternTables = new javax.swing.JPanel();
         jBtnShowPT = new javax.swing.JButton();
+        jBtnShowNext = new javax.swing.JButton();
+        jBtnRefresh = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Table Pattern Viewer");
@@ -70,25 +74,36 @@ public class PatternTableViewer extends javax.swing.JFrame {
             .addGap(0, 338, Short.MAX_VALUE)
         );
 
-        jBtnShowPT.setText("Show Pattern Table");
+        jBtnShowPT.setText("Show Pattern Table In Memory & Refresh");
         jBtnShowPT.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBtnShowPTActionPerformed(evt);
             }
         });
 
+        jBtnShowNext.setText(">> Show Next Table on ROM File");
+        jBtnShowNext.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBtnShowNextActionPerformed(evt);
+            }
+        });
+
+        jBtnRefresh.setText("Refresh Pattern on ROM");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPnPatternTables, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jPnPatternTables, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(300, 300, 300)
-                        .addComponent(jBtnShowPT)))
+                        .addComponent(jBtnShowPT)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jBtnShowNext)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jBtnRefresh)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -97,7 +112,10 @@ public class PatternTableViewer extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jPnPatternTables, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jBtnShowPT)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jBtnShowPT)
+                    .addComponent(jBtnShowNext)
+                    .addComponent(jBtnRefresh))
                 .addContainerGap())
         );
 
@@ -131,11 +149,47 @@ public class PatternTableViewer extends javax.swing.JFrame {
                     indexY += 9 * 2;
                 }
             }
-            if (z==254){
+            if (z == 254) {
                 indexY += 9 * 2 * 4;
             }
         }
 }//GEN-LAST:event_jBtnShowPTActionPerformed
+    private int actualChrPage = 0;
+    private void jBtnShowNextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnShowNextActionPerformed
+
+        int addressInitial = (actualChrPage == 0) ? 0 : actualChrPage * 8 * 1024 + 1;
+        Map<Integer, int[][]> patternTable = Emulator.getInstance().getPPU().getPatternTable(
+                Arrays.copyOfRange(Emulator.getInstance().rom().chr_rom, actualChrPage, ((actualChrPage == 0) ? 1 : actualChrPage) * 8 * 1024));
+        actualChrPage++;
+        if (actualChrPage > Emulator.getInstance().rom().CHR_ROMPageCount8K * 8 * 1024) {
+            actualChrPage = 0;
+        }
+
+        Graphics scr = jPnPatternTables.getGraphics();
+        int indexX = 0, indexY = 0;
+        for (int z = 0; z < 512; z++) {
+            int[][] tile = patternTable.get(z);
+            int theLastX = indexX;
+            for (byte row = 0; row < 8; row++) {
+                indexX = theLastX;
+                for (byte collumn = 7; collumn >= 0; collumn--, indexX++) {
+                    scr.setColor(colors.get(tile[row][collumn]));
+                    scr.fillRect(indexX * 2, indexY + row * 2, 2, 2);
+                }
+            }
+            indexX = theLastX;
+            indexX += 9;
+            if (z != 0) {
+                if (z % 38 == 0) {
+                    indexX = 0;
+                    indexY += 9 * 2;
+                }
+            }
+            if (z == 254) {
+                indexY += 9 * 2 * 4;
+            }
+        }
+}//GEN-LAST:event_jBtnShowNextActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -147,6 +201,8 @@ public class PatternTableViewer extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jBtnRefresh;
+    private javax.swing.JButton jBtnShowNext;
     private javax.swing.JButton jBtnShowPT;
     private javax.swing.JPanel jPnPatternTables;
     // End of variables declaration//GEN-END:variables
