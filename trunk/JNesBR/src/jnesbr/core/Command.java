a42d2e48b935727915a3acb82d7eafec4d5b4330
leandro.rhcp99 <leandro.rhcp99@4cb0bef4-e3dc-11dd-9759-139c9ab14fc6>
2009-05-09 03:12:41 +0000

    mirroring for the games without mapper working and tested. (finally)

---
+++ trunk/JNesBR/src/jnesbr/core/Command.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jnesbr.core;
+
+/**
+ *
+ * @author Leandro
+ */
+public interface Command {
+    int execute(int address);
+}
